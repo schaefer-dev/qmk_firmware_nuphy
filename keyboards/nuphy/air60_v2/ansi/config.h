5c8fa0cd38d948c88d17507768c99ff72ec63cb1
Daniel Schäfer <schaefer.dev@gmail.com>
2024-02-29 20:44:26 +0100

    Implemented nuphy air60v2 layout with reduced delay for tapdance

--- keyboards/nuphy/air60_v2/ansi/config.h
+++ keyboards/nuphy/air60_v2/ansi/config.h
@@ -78,3 +78,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_KEYRELEASES
 
 
+#define TAPPING_TERM 80 // default anyways
+#define PERMISSIVE_HOLD //  immediately selects the hold action when another key is tapped (pressed and then released) while the dual-role key is held down
